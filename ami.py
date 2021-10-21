544f495a35296c59957199bcfdba7ab9b8509f57
Takuya ASADA <syuu@scylladb.com>
2021-10-22 02:41:19 +0900

    build with ubuntu mode dpackager

--- ami.py
+++ ami.py
@@ -2,23 +2,29 @@ import os
 import sys
 import re
 import shutil
+import argparse
 from subprocess import run
 
-print('--- enviroment variables ---')
-print(os.environ)
-print('------')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--repo')
+    args = parser.parse_args()
 
-repo = 'http://downloads.scylladb.com/unstable/scylla/master/deb/unified/2021-08-26T12:02:36Z/scylladb-master/scylla.list'
+    print('--- enviroment variables ---')
+    print(os.environ)
+    print('------')
 
-shutil.copyfile('./json_files/ami_variables.json', './scylla-machine-image/aws/ami/variables.json')
-os.chdir('./scylla-machine-image')
-run(f'../tools/packaging/dpackager ./packer/build_deb_image.sh --product scylla --repo {repo} --log-file ami.log', shell=True, check=True)
-with open('./aws/ami/build/ami.log') as f:
-    ami_log = f.read()
-match = re.search(r'^us-east-1: (.+)$', ami_log, flags=re.MULTILINE)
-if not match:
-    print('AMI build failed')
-    sys.exit(1)
+    shutil.copyfile('./json_files/ami_variables.json', './scylla-machine-image/aws/ami/variables.json')
+    ami_env = os.environ.copy()
+    ami_env['DOCKER_IMAGE'] = 'image_ubuntu20.04'
+    run(f'../../../tools/packaging/dpackager ./build_deb_ami.sh --product scylla --repo {args.repo} --log-file ami.log', cwd='./scylla-machine-image/aws/ami', shell=True, check=True, env=ami_env)
+    with open('./scylla-machine-image/aws/ami/build/ami.log') as f:
+        ami_log = f.read()
+    match = re.search(r'^us-east-1: (.+)$', ami_log, flags=re.MULTILINE)
+    if not match:
+        print('AMI build failed')
+        sys.exit(1)
 
-scylla_ami_id = match.group(1)
-print(f'scylla_ami_id={scylla_ami_id}')
+    scylla_ami_id = match.group(1)
+    with open('./amiId.properties', 'w') as f:
+        f.write(f'scylla_ami_id={scylla_ami_id}')
