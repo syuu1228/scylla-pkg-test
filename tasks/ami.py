5aeab6950eca676fde7160ea2d3d04b30141da9b
Takuya ASADA <syuu@scylladb.com>
2021-10-24 12:12:30 +0900

    ami: automatically resolve repoUrl

--- tasks/ami.py
+++ tasks/ami.py
@@ -6,35 +6,63 @@ import shutil
 from invoke import task, Collection
 from rich import print  # pylint: disable=redefined-builtin
 from rich.console import Console
+import requests
 
 console = Console(color_system=None)
 
-@task()
-def build(c, repo, distro):
-    shutil.copyfile('./json_files/ami_variables.json', './scylla-machine-image/aws/ami/variables.json')
-    ami_env = os.environ.copy()
-    ami_env['DPACKAGER_TOOL'] = 'podman'
-    if distro == 'ubuntu:20.04':
-        ami_env['DOCKER_IMAGE'] = 'image_ubuntu20.04'
-        script_name = './build_deb_ami.sh'
-    elif distro == 'centos:7':
-        ami_env['DOCKER_IMAGE'] = 'image_fedora-33'
-        script_name = './build_ami.sh'
-    else:
+def check_distro(distro):
+    if distro != 'ubuntu:20.04' and distro != 'centos:7':
         raise Exception('Unsupported distro')
-    with c.cd('./scylla-machine-image/aws/ami'):
-        c.run(f'../../../tools/packaging/dpackager -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -- {script_name} --product scylla --repo {repo} --log-file build/ami.log', env=ami_env)
-    with open('./scylla-machine-image/aws/ami/build/ami.log') as f:
-        ami_log = f.read()
-    match = re.search(r'^us-east-1: (.+)$', ami_log, flags=re.MULTILINE)
-    if not match:
-        raise Exception('AMI build failed')
-    scylla_ami_id = match.group(1)
-    with open('./amiId.properties', 'w') as f:
-        f.write(f'scylla_ami_id={scylla_ami_id}')
 
 @task()
-def generate_properties(c, ami_id):
+def fetch_repo_url(c, job_name, build_num, artifact_url, distro):
+    check_distro(distro)
+    if artifact_url == 'latest':
+        if not job_name:
+            if distro == 'ubuntu:20.04':
+                job_name = '/scylla-master/job/unified-deb'
+            elif distro = 'centos:7':
+                job_name = '/scylla-master/job/centos-rpm'
+        if not build_num:
+            build_num = 'lastSuccessfulBuild'
+        with open('/var/tmp/takuya-api-token.txt') as f:
+            token = f.read().strip()
+        r = requests.get(f'https://jenkins.scylladb.com/view/master/job${job_name}/${build_num}/artifact/00-Build.txt', auth=('syuu1228',token))
+        build_info = r.text
+        if distro == 'ubuntu:20.04':
+            pattern = r'^unified-deb-url: (.+)$'
+        elif distro == 'centos:7':
+            pattern = r'^centos-rpm-url: (.+)$'
+        match = re.search(pattern, metadata, flags=re.MULTILINE)
+        if not match:
+            raise Exception('repository URL not found')
+        artifact_url = match.group(1)
+    if distro == 'ubuntu:20.04':
+        print(f'http://{artifact_url}/scylladb-master/scylla.list')
+    else:
+        print(f'http://{artifact_url}/scylla.repo')
+
+@task()
+def build(c, repo, distro, ami_id):
+    check_distro(distro)
+    if not ami_id:
+        shutil.copyfile('./json_files/ami_variables.json', './scylla-machine-image/aws/ami/variables.json')
+        ami_env = os.environ.copy()
+        ami_env['DPACKAGER_TOOL'] = 'podman'
+        if distro == 'ubuntu:20.04':
+            ami_env['DOCKER_IMAGE'] = 'image_ubuntu20.04'
+            script_name = './build_deb_ami.sh'
+        elif distro == 'centos:7':
+            ami_env['DOCKER_IMAGE'] = 'image_fedora-33'
+            script_name = './build_ami.sh'
+        with c.cd('./scylla-machine-image/aws/ami'):
+            c.run(f'../../../tools/packaging/dpackager -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -- {script_name} --product scylla --repo {repo} --log-file build/ami.log', env=ami_env)
+        with open('./scylla-machine-image/aws/ami/build/ami.log') as f:
+            ami_log = f.read()
+        match = re.search(r'^us-east-1: (.+)$', ami_log, flags=re.MULTILINE)
+        if not match:
+            raise Exception('AMI build failed')
+        ami_id = match.group(1)
     with open('./amiId.properties', 'w') as f:
         f.write(f'scylla_ami_id={ami_id}')
 
@@ -52,4 +80,4 @@ def test(c, ami_id):
     with c.cd('./scylla-cluster-tests'):
         c.run('./docker/env/hydra.sh run-test artifacts_test --backend aws', env=sct_env, pty=True)
 
-ns = Collection(build, generate_properties, test)
+ns = Collection(fetch_repo_url, build, test)
