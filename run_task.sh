619b28f8bd72d3d254598446cc1d1ba7c7bf1159
Takuya ASADA <syuu@scylladb.com>
2021-10-25 20:02:41 +0900

    run_task.sh: don't run virtualenv if aleady executed

---
+++ run_task.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [ ! -d poc-env ]; then
+    pip3 install virtualenv
+    python3 -m virtualenv poc-venv
+fi
+source poc-venv/bin/activate && pip3 install invoke rich requests
+poc-venv/bin/invoke "$@"
