4c4760d70862797a46db158eacb106b3f34ff675
Takuya ASADA <syuu@scylladb.com>
2021-10-26 05:41:00 +0900

    remoge GitPython

--- run_task.sh
+++ run_task.sh
@@ -2,5 +2,5 @@
 
 pip3 install virtualenv
 python3 -m virtualenv poc-venv
-source poc-venv/bin/activate && pip3 install invoke pydantic requests GitPython
+source poc-venv/bin/activate && pip3 install invoke pydantic requests
 poc-venv/bin/invoke "$@"
