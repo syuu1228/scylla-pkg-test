20cc09eae845dc1e10adc4581bede92bc262df50
Takuya ASADA <syuu@scylladb.com>
2021-10-26 06:16:09 +0900

    fix error

--- tasks/ami.py
+++ tasks/ami.py
@@ -36,17 +36,13 @@ def dpackager(cmdline, topdir, image='image_fedora-33', env_export={}, env_overw
     denv.update(env_overwrite)
     env_export_arg=''
     for e in env_export:
-        env_export_arg = ' -e {e}=${e}'
+        env_export_arg = f' -e {e}=${e}'
     encoding = 'utf-8' if capture_output else None
     try:
         return subprocess.run(f"{topdir}/tools/packaging/dpackager {env_export_arg} -- {cmdline}", shell=True, check=True, cwd=cwd, env=denv, capture_output=capture_output, encoding=encoding)
     except subprocess.CalledProcessError as e:
         print(f'args:{e.args}')
         print(f'returncode:{e.returncode}')
-        print('stdout:')
-        print(e.stdout)
-        print('stderr:')
-        print(e.stderr)
         raise
 
 @task
